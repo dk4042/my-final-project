54979fbd76e49ce0efb9a0a486b1a1c659a725d1
dk4042 <dkhamaza@uchicago.edu>
2024-12-04 05:53:58 +0000

    updating and deleting fuctions added

--- app/controllers/coffee_controller.rb
+++ app/controllers/coffee_controller.rb
@@ -3,6 +3,42 @@ class CoffeeController < ApplicationController
     render({ :template => "home_templates/home"})
   end
 
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_reviews = Review.where({ :id => the_id })
+    @the_review = matching_reviews.at(0)
+
+    render({ :template => "reviews_templates/show" })
+  end
+
+  def update 
+    the_id = params.fetch("the_id")
+    matching_records = Review.where({:id => the_id})
+    the_review = matching_records.at(0)
+  
+    the_review.name = params.fetch("the_name")
+    the_review.region = params.fetch("the_region")
+    the_review.review = params.fetch("the_review")
+    the_review.image = params.fetch("the_image")
+  
+    the_review.save
+  
+    redirect_to("/coffee_reviews")
+  end
+
+  def destroy
+    the_id = params.fetch("the_id")
+    matching_records = Review.where({ :id => the_id })
+    the_review = matching_records.at(0)
+    if the_review
+      the_review.destroy
+      redirect_to("/coffee_reviews")
+    else
+      redirect_to("/coffee_reviews", alert: "Review not found.")
+    end
+  end
+
   def create
     a = Review.new
     a.name = params.fetch("the_name")
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get("/", { :controller => "coffee", :action => "homepage" })
 
   get("/coffee_reviews", { :controller => "coffee", :action => "reviews" })
+
+  get("/coffee_reviews/:the_id", { :controller => "coffee", :action => "show" })
   post("/insert_review", { :controller => "coffee", :action => "create" } )
+
+  get("/delete_review/:the_id", { :controller => "coffee", :action => "destroy" }  )
+  post("/modify_review/:the_id", { :controller => "coffee", :action => "update" } )
   
 end
