c01bf3e0617dba7e841675d369ddb81d8302dfdb
dk4042 <dkhamaza@uchicago.edu>
2024-12-05 18:38:47 +0000

    added the rank feature

--- app/controllers/coffee_controller.rb
+++ app/controllers/coffee_controller.rb
@@ -19,6 +19,7 @@ class CoffeeController < ApplicationController
     the_review.region = params.fetch("the_region")
     the_review.review = params.fetch("the_review")
     the_review.image = params.fetch("the_image")
+    the_review.rank = params.fetch("the_rank")
   
     the_review.save
   
@@ -43,6 +44,7 @@ class CoffeeController < ApplicationController
     a.region = params.fetch("the_region")
     a.review = params.fetch("the_review")
     a.image = params.fetch("the_image")
+    a.rank = params.fetch("the_rank")
    
     a.user_id = current_user.id
     a.save
